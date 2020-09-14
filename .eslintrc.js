f66567d3b50dae3e8da58cbab3309d7d0cd4ffed
nielsja <nielsja@users.noreply.github.com>
2020-09-14 15:20:03 -0500

    Added ESLint and updated formatter config files

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/*global module */
+/*esLint no-undef: "error"*/
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+};
